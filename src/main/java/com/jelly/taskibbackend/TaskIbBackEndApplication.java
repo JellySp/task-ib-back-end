bb426b23d4f59d5e1e70a3bea8ee2a7d177465c2
JoosepA <j_andrespuk@hotmail.com>
2021-02-26 07:08:02 +0200

    - initial commit

---
+++ src/main/java/com/jelly/taskibbackend/TaskIbBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.jelly.taskibbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskIbBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskIbBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jelly/taskibbackend/TaskIbBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jelly.taskibbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskIbBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
