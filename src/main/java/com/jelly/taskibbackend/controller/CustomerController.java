4f88dd86c45e59c332444537a92821a562cb8ded
j_and <j_andrespuk@hotmail.com>
2021-03-03 18:47:47 +0200

    - LoanParametersChecker

--- src/main/java/com/jelly/taskibbackend/controller/CustomerController.java
+++ src/main/java/com/jelly/taskibbackend/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.jelly.taskibbackend.controller;
 
 import com.jelly.taskibbackend.model.Customer;
 import com.jelly.taskibbackend.repository.CustomerRepository;
+import com.jelly.taskibbackend.utils.LoanParametersChecker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,27 +38,16 @@ public class CustomerController {
     @GetMapping("/verifyCustomerData")
     public Boolean verifyCustomerData(@RequestParam String pic,@RequestParam double loanAmount, @RequestParam double loanPeriod) {
 
-        // this functions rechecks the credit score after applying for loan to prevent the customer from injecting a
+        // this functions rechecks the credit
+        // score after applying for loan to
+        // prevent the customer from injecting
         // bad values into their browser
+
         Customer customer = customerRepository.findByPic(pic);
-        return isCorrectCreditScore(customer.getCreditModifier(), loanAmount, loanPeriod) && isCorrectLoanAmount(loanAmount) && isCorrectLoanPeriod(loanPeriod);
+        return LoanParametersChecker.isCorrectParameters(customer, loanAmount, loanPeriod);
     }
 
 
-    //not entirely sure if I should be having  non mapped methods in this class but without using these,
-    // the isCorrectData() method would look super ugly
-
-    private boolean isCorrectCreditScore(int creditModifier, double loanAmount, double loanPeriod) {
-        return ((creditModifier / loanAmount) * loanPeriod) >= 1;
-    }
-
-    private boolean isCorrectLoanAmount(double loanAmount) {
-        return loanAmount >= 2000 && loanAmount <= 10_000;
-    }
-
-    private boolean isCorrectLoanPeriod(double loanPeriod) {
-        return loanPeriod >= 12 && loanPeriod <= 60;
-    }
 
 
 
--- src/test/java/com/jelly/taskibbackend/controller/CustomerControllerTest.java
+++
@@ -1,6 +0,0 @@
-package com.jelly.taskibbackend.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-class CustomerControllerTest {
-
-}
