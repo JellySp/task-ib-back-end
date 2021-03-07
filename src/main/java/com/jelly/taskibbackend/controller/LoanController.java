6782564beeed1ca8c5952dc0a6bfef28d0ea42db
j_and <j_andrespuk@hotmail.com>
2021-03-07 06:25:19 +0200

    - loan parameters validation

--- src/main/java/com/jelly/taskibbackend/controller/LoanController.java
+++ src/main/java/com/jelly/taskibbackend/controller/LoanController.java
@@ -1,14 +1,20 @@
 package com.jelly.taskibbackend.controller;
 
-import com.jelly.taskibbackend.model.Customer;
 import com.jelly.taskibbackend.model.LoanOffer;
 import com.jelly.taskibbackend.utils.LoanCalculator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 
 @CrossOrigin
 @RestController
 @RequestMapping
+@Validated
 public class LoanController {
 
     @Autowired
@@ -17,7 +23,7 @@ public class LoanController {
 
 
     @GetMapping("/getLoanOffer")
-    public LoanOffer findByPic(@RequestParam String pic, @RequestParam double loanAmount, double loanPeriod) {
+    public LoanOffer getLoanOffer(@RequestParam String pic, @RequestParam @Min(2000) @Max(10000) double loanAmount, @RequestParam @Min(12) @Max(60) double loanPeriod) {
         LoanCalculator loanCalculator = new LoanCalculator();
 
         return loanCalculator.getLoanOffer(customerController.findByPic(pic), loanAmount, loanPeriod);
